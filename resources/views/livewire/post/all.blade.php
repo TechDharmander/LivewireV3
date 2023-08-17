44d312edf1f78d823cc1fd264af7e356e389346b
Dharmander Singh <dharmander.switspl@gmail.com>
2023-08-17 08:50:41 +0530

    All post display

--- resources/views/livewire/post/all.blade.php
+++ resources/views/livewire/post/all.blade.php
@@ -4,7 +4,49 @@
             <div class="card">
                 <div class="card-header">Post Lists</div>
                 <div class="card-body" style="color:white;">
-                    {{ dd($allPosts) }}
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered">
+                            <thead class="bg-secondary">
+                                <tr>
+                                    <th>#</th>
+                                    <th>Title</th>
+                                    <th>Slug</th>
+                                    <th>Created At</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Hello</td>
+                                    <td>hello</td>
+                                    <td>01-05-2023</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Hello</td>
+                                    <td>hello</td>
+                                    <td>01-05-2023</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Hello</td>
+                                    <td>hello</td>
+                                    <td>01-05-2023</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>Hello</td>
+                                    <td>hello</td>
+                                    <td>01-05-2023</td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
